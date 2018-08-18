6235af06258b3d9957f4828c08818f55e8e9c89b
Artur Khabirov <gooolpe@gmail.com>
2018-08-18 15:58:42 +0500

    social

--- src/components/HomePage/social.js
+++ src/components/HomePage/social.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Social(props){
+class Social extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
     return (
 	    	<section id="social" className="pt-5 pb-5" >
 	    		<div className="container text-white">
 	    			<h2>RANDOM RULES В СОЦСЕТЯХ: 
-						<a href="" className="text-white"><i className="fab fa-vk fa-2x ml-4 mr-4" ></i></a>
-						<a href="" className="text-white"><i className="fab fa-discord fa-2x  mr-4"></i></a>
-						<a href="" className="text-white"><i className="fab fa-youtube fa-2x mr-4"></i></a>
-						<a href="" className="text-white"><i className="fab fa-patreon fa-2x mr-4"></i></a>
-						<a href="" className="text-white"><i className="fab fa-telegram-plane fa-2x mr-4"></i></a>
+						<a href="https://vk.com/rrules" target="_blank" data-toggle="tooltip" data-placement="bottom" title="Вконтакте" className="text-white"><i className="fab fa-vk fa-2x ml-2 mr-2" ></i></a>
+						<a href="https://discordapp.com/invite/jPfXJ2s" data-toggle="tooltip" data-placement="bottom" title="Discord" target="_blank" className="text-white"><i className="fab fa-discord fa-2x  ml-2 mr-2"></i></a>
+						<a href="https://www.youtube.com/randomrulez" data-toggle="tooltip" data-placement="bottom" title="Youtube" target="_blank" className="text-white"><i className="fab fa-youtube fa-2x ml-2 mr-2"></i></a>
+						<a href="https://www.patreon.com/randomrules" data-toggle="tooltip" data-placement="bottom" title="Patreon" target="_blank" className="text-white"><i className="fab fa-patreon fa-2x ml-2 mr-2"></i></a>
+						<a href="https://t.me/randomrules" data-toggle="tooltip" data-placement="bottom" title="Telegram" target="_blank" className="text-white"><i className="fab fa-telegram-plane fa-2x ml-2 mr-2"></i></a>
+						<a href="https://www.twitch.tv/random_rules" data-toggle="tooltip" data-placement="bottom" title="Twitch" target="_blank" className="text-white"><i className="fab fa-twitch fa-2x ml-2 mr-2"></i></a>
 					</h2>
 				</div>
 	    	</section>
     );
 }
-
+}
 export default Social;
