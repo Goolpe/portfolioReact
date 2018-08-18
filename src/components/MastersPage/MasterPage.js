b8fa76bd1894b13a3363e21aea04ea5b3531a5f8
Artur Khabirov <gooolpe@gmail.com>
2018-08-18 05:03:23 +0500

    masters

--- src/components/MastersPage/MasterPage.js
+++ src/components/MastersPage/MasterPage.js
@@ -16,32 +16,40 @@ class MasterPage extends Component {
   }
   render() {
   	const id = parseInt(this.props.match.params.id.replace(/\D+/g,""));
+    const masterYoutube = mastersJSON[id].examples.map((example, index) => 
+      <div class="col-6 mb-3">
+        <iframe
+          title={mastersJSON[id].skype}
+            src={example}
+            width="100%" 
+            height="340"
+            frameBorder="0"
+            allowFullScreen
+            key={index}>
+        </iframe>
+      </div>
+    )
 	  return (
 	  <section id="articlesPage">	  
 	  	<div className="container mt-5 mb-5">
   	  	<Link to="/masters" className="text-dark"><i className="text-dark fas fa-angle-left "></i> ДРУГИЕ МАСТЕРА</Link>
         <h1 className="text-center mb-5">{mastersJSON[id].name}</h1>
-  	  	<h3 className="mb-3">Контакты:</h3>
-            <p><span className="font-weight-bold">Discord</span> - {mastersJSON[id].discord}<br />
-        <span className="font-weight-bold">Skype</span> - {mastersJSON[id].skype}<br />
-        </p>
-        <p>
-          Водит на канале с {mastersJSON[id].date}<br />
-          {mastersJSON[id].paidGames ? "Водит" : "Не водит"} платные игры<br />
-        </p>
-        <h3 className="mb-3">О себе:</h3>
-        <p>{mastersJSON[id].text}</p>
-        <h3 className="mb-3">Любимые системы:</h3><p>{mastersJSON[id].systems}</p>
-        <h3 className="mb-3">Любимые сеттинги:</h3><p>{mastersJSON[id].setting}</p>
-        <h3 className="mb-3">Примеры игр:</h3>
-        <iframe
-          title={mastersJSON[id].skype}
-            src={mastersJSON[id].example}
-            width="70%" 
-            height="340"
-            frameBorder="0"
-            allowFullScreen>
-        </iframe>
+    	  	<h3 className="mb-4">Контакты:</h3>
+              <p><span className="font-weight-bold">Discord</span> - {mastersJSON[id].discord}<br />
+          <span className="font-weight-bold">Skype</span> - {mastersJSON[id].skype}<br />
+          </p>
+          <p>
+            Водит на канале с {mastersJSON[id].date}<br />
+            {mastersJSON[id].paidGames ? "Водит" : "Не водит"} платные игры<br />
+          </p>
+          <h3 className="mb-4">О себе:</h3>
+          <p>{mastersJSON[id].text}</p>
+          <h3 className="mb-4">Любимые системы:</h3><p>{mastersJSON[id].systems}</p>
+          <h3 className="mb-4">Любимые сеттинги:</h3><p>{mastersJSON[id].setting}</p>
+          <h3 className="mb-4">Примеры игр</h3>
+          <div className="row">
+            {masterYoutube}
+          </div>
     	</div>
     	</section>
 	  );
