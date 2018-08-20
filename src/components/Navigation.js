667213d75a4e7b50b80e5905476ee6c8924ab159
Artur Khabirov <gooolpe@gmail.com>
2018-08-20 21:18:11 +0500

    art

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -59,11 +59,11 @@ class Navigation extends Component{
                     НАВИГАЦИЯ
                   </DropdownToggle>
                   <DropdownMenu className="p-0">
-                    <DropdownItem className="p-0 rounded-top"><Link to="/library" className="p-3 text-left btn btn-light bg-transparent rounded-top w-100">БИБЛИОТЕКА</Link></DropdownItem>
-                    <DropdownItem className="p-0"><Link to="/masters" className="p-3 text-left btn btn-light bg-transparent rounded-0 w-100">РЕЗЮМЕ МАСТЕРОВ</Link></DropdownItem>
-                    <DropdownItem className="p-0"><Link to="/art" className="p-3 text-left btn btn-light bg-transparent rounded-0 w-100">АРТ</Link></DropdownItem>
-                    <DropdownItem className="p-0"><Link to="/library" className="p-3 text-left btn btn-light bg-transparent rounded-0 w-100">ПЛАТНЫЕ ИГРЫ</Link></DropdownItem>
-                    <DropdownItem className="p-0 rounded-bottom"><Link to="/library" className="p-3 text-left btn btn-light bg-transparent rounded-bottom w-100">ПОДДЕРЖАТЬ ПРОЕКТ</Link></DropdownItem>
+                    <DropdownItem className="p-0 rounded-top"><Link to="/library" className="p-3 text-left btn btn-light rounded-top w-100">БИБЛИОТЕКА</Link></DropdownItem>
+                    <DropdownItem className="p-0"><Link to="/masters" className="p-3 text-left btn btn-light rounded-0 w-100">РЕЗЮМЕ МАСТЕРОВ</Link></DropdownItem>
+                    <DropdownItem className="p-0"><Link to="/art" className="p-3 text-left btn btn-light rounded-0 w-100">АРТ</Link></DropdownItem>
+                    <DropdownItem className="p-0"><Link to="/library" className="p-3 text-left btn btn-light rounded-0 w-100">ПЛАТНЫЕ ИГРЫ</Link></DropdownItem>
+                    <DropdownItem className="p-0 rounded-bottom"><Link to="/library" className="p-3 text-left btn btn-light rounded-bottom w-100">ПОДДЕРЖАТЬ ПРОЕКТ</Link></DropdownItem>
                   </DropdownMenu>
               </Dropdown>
               <li className="nav-item">
